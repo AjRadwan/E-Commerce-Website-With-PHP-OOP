0e6f8f3571858d3f2237407df2f0b3abdca74d20
Anowar <1hafizajr@gmail.com>
2022-03-31 11:53:19 +0600

    add prodcut category

--- admin/addcat.php
+++ admin/addcat.php
@@ -1,26 +1,42 @@
 ﻿<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
-        <div class="grid_10">
-		
-            <div class="box round first grid">
-                <h2>Add New Category</h2>
-               <div class="block copyblock"> 
-                 <form>
-                    <table class="form">					
-                        <tr>
-                            <td>
-                                <input type="text" placeholder="Enter Category Name..." class="medium" />
-                            </td>
-                        </tr>
-						<tr> 
-                            <td>
-                                <input type="submit" name="submit" Value="Save" />
-                            </td>
-                        </tr>
-                    </table>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <?php include 'inc/header.php';?>
- 
+
+<?php include '../classes/Category.php';
+//create a  category object from category classes
+$cat = new Category();
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){ 
+    $catName = ($_POST['catName']);
+
+    $insertCategory = $cat->catInsert($catName);
+ }
+?>
+
+<div class="grid_10">
+<div class="box round first grid">
+    <h2>Add New Category</h2>
+    <div class="block copyblock"> 
+      <?php
+        if(isset($insertCategory)){
+            echo $insertCategory;
+        }
+      ?>  
+        <form method="post" action="">
+        <table class="form">					
+            <tr>
+                <td>
+                    <input type="text" name="catName" placeholder="Enter Category Name..." class="medium" />
+                </td>
+            </tr>
+            <tr> 
+                <td>
+                    <input type="submit" name="submit" Value="Save" />
+                </td>
+            </tr>
+        </table>
+        </form>
+    </div>
+</div>
+</div>
+<?php include 'inc/header.php';?>
+
--- classes/Category.php
+++ classes/Category.php
@@ -10,4 +10,26 @@
        $this->db = new Database();
        $this->fm = new Format();
      }
+
+     public function catInsert($catName){
+        $catName = $this->fm->validation($catName);
+        $catName = mysqli_real_escape_string($this->db->link, $catName);
+
+        if(empty($catName)){
+             $message = "<span class='error'>Category filed must not be empty!</span>";
+            return $message;
+     }else{
+         $query = "INSERT INTO tbl_category(catName) VALUES ('$catName')";
+         $catinsert = $this->db->insert($query);
+         if($catinsert){
+             $message = "<span class='success'>Category Inserted Successfully!</span>";
+             return $message;
+         }else{
+            $message = "<span class='success'>Something went wrong</span>";
+            return $message;
+           
+         }
+     }
+    }
+ }
 ?>
